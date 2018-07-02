8abc0cba03dc4ab8b7f552660bfdcb92baee9583
Kailin Huang <kailin-huang@outlook.com>
2018-07-02 16:28:11 +0200

    added a bash to run original ITM

---
+++ run_ITM_With_Test.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+
+echo "run ITM with LPD Data ..."
+/local/MT/MT_ObjSLAM/External/InfiniTAM/InfiniTAM/build/Apps/InfiniTAM/InfiniTAM /local/MT/MT_ObjSLAM/Dataset/LPD/calib_2.txt /local/MT/MT_ObjSLAM/Dataset/LPD/%04i.ppm /local/MT/MT_ObjSLAM/Dataset/LPD/%04i.pgm
