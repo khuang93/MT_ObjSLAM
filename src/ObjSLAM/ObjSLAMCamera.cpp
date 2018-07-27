9f03ffba404adb74f81b8dbeae90ad3f1cf65154
Kailin Huang <kailin-huang@outlook.com>
2018-07-27 17:30:14 +0200

    added projection img 2 world point

--- src/ObjSLAM/ObjSLAMCamera.cpp
+++ src/ObjSLAM/ObjSLAMCamera.cpp
@@ -29,7 +29,7 @@ bool ObjSLAMCamera::projectPointCloud2Img(ORUtils::Image<Vector4f> * PCL, ObjFlo
                             this->calib->intrinsics_d.projectionParamsSimple.py,
                             1.0f);
   std::cout<<"K"<<K<<std::endl;
-
+  Matrix4f pose_mat = pose.getSE3Pose().GetM();
 
 
   for(size_t i=0; i < PCL->dataSize; i++){
@@ -37,7 +37,7 @@ bool ObjSLAMCamera::projectPointCloud2Img(ORUtils::Image<Vector4f> * PCL, ObjFlo
 //    Eigen::Vector4f point_eigen(point.x, point.y, point.z, 1);
     Vector4f point = PCL->GetElement(i,MEMORYDEVICE_CPU);
 
-    Matrix4f pose_mat = pose.getSE3Pose().GetM();
+
 
     Vector4f point_camera_frame = pose_mat*point;
 
