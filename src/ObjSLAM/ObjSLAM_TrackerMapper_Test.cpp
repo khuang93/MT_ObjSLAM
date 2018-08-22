9730fa883f21ec8576063e1cfdb1e5b6fb70388d
Kailin Huang <kailin-huang@outlook.com>
2018-08-22 16:30:25 +0200

    added teddy

--- src/ObjSLAM/ObjSLAM_TrackerMapper_Test.cpp
+++ src/ObjSLAM/ObjSLAM_TrackerMapper_Test.cpp
@@ -12,11 +12,26 @@
 #include "ObjectView_New.h"
 #include <memory>
 
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/core/optimization_algorithm_dogleg.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+
+#include <g2o/types/slam3d/types_slam3d.h>
+#include <g2o/types/slam3d_addons/types_slam3d_addons.h>
+
 using namespace std;
 
 int main(int argc, char **argv) {
   //TODO Debug output
   cout << "**Hello SLAM World!" << endl;
+
+  auto * ver = new g2o::VertexSE3();
+  ver->setId(0);
+
 
   //Path of the depth image file
   string path = argv[1];
